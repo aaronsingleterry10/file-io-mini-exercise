0c90bf59a5a2cf1482e8b9834d82cfb98a79818a
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-22 12:41:58 -0500

    completed a couple steps

--- src/FileIoMiniExercise.java
+++ src/FileIoMiniExercise.java
@@ -27,10 +27,17 @@ public class FileIoMiniExercise {
         }
 
         // TODO: change "Fred" to "David" in the list of Instructor objects
-
+        List<Instructor> newInstructors = new ArrayList<>();
+        for(Instructor instructor : instructors) {
+            if (instructor.getName().equals("Fred")) {
+                instructor.setName("David");
+            }
+            newInstructors.add(instructor);
+        }
 
         // TODO: update the list of strings/names ('names' variable) to the latest names from the Instructor objects (the Instructor class has a helpful method for this)
-
+        names = Instructor.instructorsToNameStrings(newInstructors);
+        System.out.println(names);
 
         // TODO: overwrite the instructors-names.txt file with the latest list of names
 
